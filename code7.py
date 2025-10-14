0cf4c3fc3c75948f24c9f6d9834b9411676c7879
sagarika <elugotisagarika@gmail.com>
2025-10-14 19:25:19 +0530

    adding code

---
+++ code7.py
@@ -0,0 +1,17 @@
+'''#variables
+name="chinni"
+age=22
+color="blue"
+print("hi...i'm ",name,",my age is", age,"and my favourate color is",color)'''
+
+'''#printing names using print stmt
+name=input("enter ur name:")
+print(name)
+print(name)
+print(name)'''
+
+#taking input from user
+name=input("enter ur name :")
+city=input("give your city name:")
+print("my name is ",name,", i'm from ",city)
+
